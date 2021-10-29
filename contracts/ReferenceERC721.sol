1b8d5f5a112a16f63d2fd439aaf41add722b4978
Brandon Valosek <bvalosek@gmail.com>
2021-10-28 22:43:43 -0500

    change from tabs to spaces LMAO foh

--- contracts/ReferenceERC721.sol
+++ contracts/ReferenceERC721.sol
@@ -9,56 +9,56 @@ import "./HyperVIBES.sol";
 // This is an example ERC721 that will "infuse on mint"
 contract ReferenceERC721 is ERC721Enumerable {
 
-		// the minting contract needs to know
-		// - the address of the HyperVIBES protocol
-		// - the correct Realm ID
-		// - the ERC20 to infuse
-		HyperVIBES public hyperVIBES;
-		uint256 public realmId;
-		IERC20 public token;
+    // the minting contract needs to know
+    // - the address of the HyperVIBES protocol
+    // - the correct Realm ID
+    // - the ERC20 to infuse
+    HyperVIBES public hyperVIBES;
+    uint256 public realmId;
+    IERC20 public token;
 
     constructor(IERC20 token_) ERC721("MockERC721", "TEST") {
-			// token set via constructor, could be configured post-deploy as well
-			token = token_;
-		}
+      // token set via constructor, could be configured post-deploy as well
+      token = token_;
+    }
 
-		// Set the pointer to the HyperVIBES protocol
-		// NOTE: this function should be secure (only a privledged admin should be
-		// allowed to use this)
-		function setHyperVIBES(uint256 realmId_, HyperVIBES hyperVIBES_) external {
-			realmId = realmId_;
-			hyperVIBES = hyperVIBES_;
+    // Set the pointer to the HyperVIBES protocol
+    // NOTE: this function should be secure (only a privledged admin should be
+    // allowed to use this)
+    function setHyperVIBES(uint256 realmId_, HyperVIBES hyperVIBES_) external {
+      realmId = realmId_;
+      hyperVIBES = hyperVIBES_;
 
-			// we need to approve hypervibes to spend any tokens owned by this
-			// contract, we'll store the tokens required for infusing in the ERC-721
-			token.approve(address(hyperVIBES_), 2 ** 256 - 1);
-		}
+      // we need to approve hypervibes to spend any tokens owned by this
+      // contract, we'll store the tokens required for infusing in the ERC-721
+      token.approve(address(hyperVIBES_), 2 ** 256 - 1);
+    }
 
     function mint(uint256 tokenId) external {
-				// mint however you are normally
+        // mint however you are normally
         _mint(_msgSender(), tokenId);
 
-				// then infuse with hyper vibes
-				hyperVIBES.infuse(InfuseInput({
-					// must be set to configured realm
-					realmId: realmId,
+        // then infuse with hyper vibes
+        hyperVIBES.infuse(InfuseInput({
+          // must be set to configured realm
+          realmId: realmId,
 
-					// the NFT
-					collection: this,
-					tokenId: tokenId,
+          // the NFT
+          collection: this,
+          tokenId: tokenId,
 
-					// if you dont want to attribute the infusion the ERC721, the address
-					// that this is set to must call allowInfusionProxy with the ERC721
-					// address
-					infuser: address(this),
+          // if you dont want to attribute the infusion the ERC721, the address
+          // that this is set to must call allowInfusionProxy with the ERC721
+          // address
+          infuser: address(this),
 
-					// based on desired behavior and realm config
-					dailyRate: 1_000 ether,
-					amount: 100_000 ether,
+          // based on desired behavior and realm config
+          dailyRate: 1_000 ether,
+          amount: 100_000 ether,
 
-					// optional, can just be an empty string
-					comment: "nice!"
-				}));
+          // optional, can just be an empty string
+          comment: "nice!"
+        }));
     }
 
     function burn(uint256 tokenId) external {
