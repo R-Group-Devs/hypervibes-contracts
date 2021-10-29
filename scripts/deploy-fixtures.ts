b5bf3e4d4996319526fca13fdcd16f8bcbecbc02
Brandon Valosek <bvalosek@gmail.com>
2021-10-29 12:59:13 -0500

    modify deploy-fixtures

--- scripts/deploy-fixtures.ts
+++ scripts/deploy-fixtures.ts
@@ -3,33 +3,40 @@ import { ethers } from "hardhat";
 
 const { parseUnits } = utils;
 
+const BAG = parseUnits("1000000000");
+
 async function main() {
   const MockERC20 = await ethers.getContractFactory("MockERC20");
   const MockERC721 = await ethers.getContractFactory("MockERC721");
   const ReferenceERC721 = await ethers.getContractFactory("ReferenceERC721");
 
-  const [token, collection] = await Promise.all([
-    MockERC20.deploy(),
-    MockERC721.deploy(),
-  ]);
+  console.log("deploying tokens");
+  const token = await MockERC20.deploy();
+  const collection = await MockERC721.deploy();
+  await Promise.all([token.deployed(), collection.deployed()]);
+
+  console.log("minting bags");
+  const trx1 = await token.mint(parseUnits("1000000000").mul(50));
+  console.log("waiting for confirmations on mint");
+  await trx1.wait(3);
 
   // deploy 721 and give it a ton of tokens
+  console.log("deploying ref721");
   const ref721 = await ReferenceERC721.deploy(token.address);
-  await token.mint(parseUnits("1000000000"));
+  await ref721.deployed();
+  console.log("bagging the ref721");
+  await token.transfer(ref721.address, BAG);
 
   const bagRecipients = [
     "0x5b0124B9501DDAC436a41feF31Bcc173992E888a",
     "0xA34C3476ae0C4863fc39E32C0E666219503bED9F",
   ];
 
-  await Promise.all(
-    bagRecipients.map(async (addr) => {
-      const bag = parseUnits("1000000000");
-      await token.mint(bag);
-      await token.transfer(addr, bag);
-      console.log(`sent ${addr} a bag`);
-    })
-  );
+  console.log("bagging recipients");
+  for (const addr of bagRecipients) {
+    await token.transfer(addr, BAG);
+    console.log(`sent ${addr} a bag`);
+  }
 
   console.log(`MockERC20: ${token.address}`);
   console.log(`MockERC721: ${collection.address}`);
